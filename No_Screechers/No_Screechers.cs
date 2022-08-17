de451bf86c9bb2cb0332609f6cb7b1da90524b9f
Matt HB <matthbull@gmail.com>
2022-08-17 15:49:19 +0100

    init

---
+++ No_Screechers/No_Screechers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+public class No_Screechers : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod No_Screechers has been loaded!");
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod No_Screechers has been unloaded!");
+}
